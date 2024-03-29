0b8e853165d3657f064b6270f1cf3b330dad6515
touil-mohamed <touil.mohamed1@hotmail.com>
2024-03-29 14:26:42 +0100

    fix: :bug: fix index

--- BirthdayApi/index.js
+++ BirthdayApi/index.js
@@ -1,26 +1,23 @@
 const http = require("http");
 const url = require("url");
-const fs = require("fs");
-const path = require("path");
-const mime = require("mime-types");
-const swaggerUiPath = require("swagger-ui-dist").getAbsoluteFSPath();
 
-const classRoute = require("./routes/class.routes");
-const studentRoute = require("./routes/student.routes");
+const giftRoute = require("./routes/gift.routes");
+const giftListRoute = require("./routes/giftList.routes")
 
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
 
-  if (parsedUrl.pathname.startsWith("/Gift")) {
-    classRoute.handleRoutes(req, res);
-  } else if (parsedUrl.pathname.startsWith("/Student")) {
-    studentRoute.handleRoutes(req, res);
+  if (parsedUrl.pathname.startsWith("/GiftsList")) {
+    giftListRoute.handleRoutes(req, res);
+    
+  } else if (parsedUrl.pathname.startsWith("/Gifts")) {
+    giftRoute.handleRoutes(req, res);
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("Not Found");
   }
 });
 
-server.listen(3000, () => {
-  console.log("Server listening on http://localhost:3000");
+server.listen(3001, () => {
+  console.log("Server listening on http://localhost:3001");
 });
